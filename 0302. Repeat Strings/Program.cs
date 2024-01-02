f4a6c709a4ccd908d5917d23a1a19f4ab6561927
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2024-01-02 18:57:52 +0200

    0302. Repeat Strings

---
+++ 0302. Repeat Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _0302._Repeat_Strings
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArr = Console.ReadLine()
+                 .Split()
+                 .ToArray();
+
+            for (int i = 0; i <= stringArr.Length - 1; i++)
+            {
+                string currentString = stringArr[i];
+                for (int j = 0; j <= currentString.Length - 1; j++)
+                {
+                    Console.Write(currentString);
+                }
+            }
+        }
+    }
+}
