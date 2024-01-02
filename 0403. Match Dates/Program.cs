aadef86c94af9105bac1baabc3440ac849342efa
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2024-01-02 19:06:48 +0200

    0403. Match Dates

---
+++ 0403. Match Dates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _0403._Match_Dates
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<day>\b\d{2})(?<Separator>[\/\-\.])(?<month>[A-Z][a-z]{2})\k<Separator>(?<year>\d{4}\b)";
+            Regex regex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            MatchCollection validNames = regex.Matches(text);
+
+            //Console.WriteLine(string.Join(", ", validNames));
+
+            foreach (Match name in validNames)
+            {
+                Console.WriteLine($"Day: {name.Groups["day"]}, Month: {name.Groups["month"]}, Year: {name.Groups["year"]}");
+            }
+        }
+    }
+}
