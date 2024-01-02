cb644106cdb74489d8aae240c7c06afb80ade16b
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2024-01-02 19:05:30 +0200

    0402. Match Phone Number

---
+++ 0402. Match Phone Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _0402._Match_Phone_Number
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359(?<Separator>[\ \-])2\k<Separator>\d{3}\k<Separator>\d{4}\b";
+            Regex regex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            MatchCollection validNames = regex.Matches(text);
+
+            Console.WriteLine(string.Join(", ", validNames));
+
+            //foreach (Match name in validNames)
+            //{
+            //    Console.Write($"{name.Value}, ");
+            //}
+            //Console.WriteLine();
+        }
+    }
+}
