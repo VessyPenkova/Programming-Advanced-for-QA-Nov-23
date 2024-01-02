69a64f609b3129f31d993a9105a9f0ba52d1a25f
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2024-01-02 19:04:23 +0200

    0401. Match Full Name

---
+++ 0401. Match Full Name/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _0401._Match_Full_Name
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+            Regex regex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            MatchCollection validNames = regex.Matches(text);
+
+            //Console.WriteLine(string.Join(" ", validNames));
+
+            foreach (Match name in validNames)
+            {
+                Console.Write($"{name.Value} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
