af6c2d9df90441494e8e5300f218ff41c3955c0b
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2024-01-02 19:00:47 +0200

    0304. Text Filter

---
+++ 0304. Text Filter/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _0304._Text_Filter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine()
+                 .Split(", ")
+                 .ToArray();
+
+            string stringToSubstring = Console.ReadLine();
+
+            foreach (var bannedWord in bannedWords)
+            {
+                if (stringToSubstring.Contains(bannedWord))
+                {
+                    stringToSubstring = stringToSubstring.Replace(bannedWord,
+                        new string('*', bannedWord.Length));
+                }
+            }
+            Console.WriteLine(stringToSubstring);
+        }
+    }
+}
