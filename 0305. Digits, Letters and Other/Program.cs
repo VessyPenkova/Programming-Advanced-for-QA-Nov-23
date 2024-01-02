a99e490612309ccd56cbd00c52b8e7fc7f9cc75e
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2024-01-02 19:01:56 +0200

    0305. Digits, Letters and Other

---
+++ 0305. Digits, Letters and Other/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _0305._Digits__Letters_and_Other
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToCheck = Console.ReadLine();
+
+            List<int> integersList = new List<int>();
+            List<char> letters = new List<char>();
+            List<char> simbols = new List<char>();
+
+            foreach (char item in stringToCheck)
+            {
+                if (char.IsLetter(item))
+                {
+                    letters.Add(item);
+                }
+                if (char.IsDigit(item))
+                {
+                    integersList.Add((item - '0'));
+                }
+                if (!char.IsLetterOrDigit(item))
+                {
+                    simbols.Add(item);
+                }
+            }
+            Console.WriteLine(String.Join((""), integersList));
+            Console.WriteLine(String.Join((""), letters));
+            Console.WriteLine(String.Join((""), simbols));
+        }
+    }
+}
