e0490a140fe78f753a64a10a199eb13a53e4cb38
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2024-01-02 18:54:01 +0200

    Add project files.

---
+++ 0301. Reverse Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _0301._Reverse_Strings
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = String.Empty;
+            // string reversedWord = String.Empty;
+
+            List<string> wordList = new List<string>();
+            List<string> reversedWorList = new List<string>();
+
+            while (word != "end")
+            {
+                word = Console.ReadLine();
+
+                if (word == "end")
+                {
+                    break;
+                }
+
+                char[] charArray = word.ToCharArray();
+                Array.Reverse(charArray);
+                string reversedWord = new string(charArray);
+
+                Console.WriteLine($"{word} = {reversedWord}");
+
+            }
+        }
+    }
+}
---
+++ Programming Advanced for QA  String and Text Processung - Lab/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Programming_Advanced_for_QA__String_and_Text_Processung___Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
