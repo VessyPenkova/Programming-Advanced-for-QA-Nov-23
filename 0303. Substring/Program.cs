dd798467bc4b292d384e0ca9ed796dd3d73ef84e
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2024-01-02 18:59:18 +0200

    0303. Substring

---
+++ 0303. Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _0303._Substring
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = Console.ReadLine();
+            string secondString = Console.ReadLine();
+
+            int index = secondString.IndexOf(firstString);
+
+            while (index != -1)
+            {
+                secondString = secondString.Remove(index, firstString.Length);
+                index = secondString.IndexOf(firstString);
+            }
+
+            Console.WriteLine(secondString);
+        }
+    }
+}
